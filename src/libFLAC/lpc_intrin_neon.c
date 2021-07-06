09c5cb418329537aa5cf5b9688bce7b261de4adb
Martijn van Beurden <mvanb1@gmail.com>
2021-07-06 15:55:47 +0200

    Change NEON routines to double, cmake and configure fixes
    This commit changes the NEON routines to use doubles. Also,
    recently added cmake code was fixed, and NEON A64 routine
    detection was added to cmake and configure

--- src/libFLAC/lpc_intrin_neon.c
+++ src/libFLAC/lpc_intrin_neon.c
@@ -2,33 +2,19 @@
 
 #ifndef FLAC__INTEGER_ONLY_LIBRARY
 #ifndef FLAC__NO_ASM
-#if defined FLAC__CPU_ARM && FLAC__HAS_NEONINTRIN
+#if defined FLAC__CPU_AARCH64 && FLAC__HAS_A64NEONINTRIN
 #include "private/lpc.h"
 #include "FLAC/assert.h"
 #include "FLAC/format.h"
 #include <arm_neon.h>
 
 /*
-*   Vec     [1 , 2 , 3 , 4]
-*   Result  [1 , 1 , 1 , 1]
-*/
-// Set all vector lanes to the value of lane 0 
-float32x4_t shufffleVector(float32x4_t vec)
-{
-    float32_t lane0 = vgetq_lane_f32(vec, 0);
-    vec = vsetq_lane_f32(lane0, vec, 1);
-    vec = vsetq_lane_f32(lane0, vec, 2);
-    vec = vsetq_lane_f32(lane0, vec, 3);
-
-    return vec;
-}
-/*
 *   Index   [0 , 1 , 2 , 3]
 *   Mask    [3 , 0 , 1 , 2]
 *   Vec     [1 , 2 , 3 , 4]
 *   Result  [4 , 1 , 2 , 3]
 */
-float32x4_t shufffleVector_3012(float32x4_t vec)
+inline float32x4_t shufffleVector_3012(float32x4_t vec)
 {
     float32_t lane0 = vgetq_lane_f32(vec, 0);
     float32_t lane1 = vgetq_lane_f32(vec, 1);
@@ -47,18 +33,20 @@ float32x4_t shufffleVector_3012(float32x4_t vec)
 *   Dest    [1 , 2 , 3 , 4]
 *   Result  [4 , 2 , 3 , 4]
 */
-float32x4_t copyLane(float32x4_t dest, float32x4_t source)
+inline float32x4_t copyLane(float32x4_t dest, float32x4_t source)
 {
     float32_t lane0 = vgetq_lane_f32(source, 0);
     dest = vsetq_lane_f32(lane0, dest, 0);
     return dest;
 }
 
-void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_4(const FLAC__real data[], uint32_t data_len, uint32_t lag, FLAC__real autoc[])
+void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_4(const FLAC__real data[], uint32_t data_len, uint32_t lag, double autoc[])
 {
     int i;
     int limit = data_len - 4;
-    float32x4_t sum0 = vdupq_n_f32(0.0f);
+    float64x2_t sum0 = vdupq_n_f64(0.0f);
+    float64x2_t sum1 = vdupq_n_f64(0.0f);
+    float32x4_t sumtemp = vdupq_n_f32(0.0f);
 
     (void)lag;
     FLAC__ASSERT(lag <= 4);
@@ -68,8 +56,11 @@ void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_4(const FLAC__real data[]
     {
         float32x4_t d, d0;
         d0 = vld1q_f32(data + i);
-        d = shufffleVector(d0);
-        sum0 = vaddq_f32(sum0, vmulq_f32(d0, d));
+        d = vdupq_n_f32(vgetq_lane_f32(d0, 0));
+
+        sumtemp = vmulq_f32(d0, d);
+        sum0 = vaddq_f64(sum0, vcvt_f64_f32(vget_low_f32(sumtemp)));
+        sum1 = vaddq_f64(sum1, vcvt_f64_f32(vget_high_f32(sumtemp)));
     }
 
     {
@@ -78,26 +69,30 @@ void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_4(const FLAC__real data[]
         if (limit < 0)
             limit = 0;
 
-        for (; i < (long)data_len; i++)
+        for (i = data_len - 1; i >= limit; i--)
         {
-            float32x4_t d = vld1q_lane_f32(data + i, vdupq_n_f32(0.0f), 0);
-
-            d = shufffleVector(d);
-
+            float32x4_t d = vdupq_n_f32(data[i]);
             d0 = shufffleVector_3012(d0);
             d0 = copyLane(d0, d);
-            sum0 = vaddq_f32(sum0, vmulq_f32(d, d0));
+
+            sumtemp = vmulq_f32(d0, d);
+            sum0 = vaddq_f64(sum0, vcvt_f64_f32(vget_low_f32(sumtemp)));
+            sum1 = vaddq_f64(sum1, vcvt_f64_f32(vget_high_f32(sumtemp)));
         }
     }
-    vst1q_f32(autoc, sum0);
+    vst1q_f64(autoc, sum0);
+    vst1q_f64(autoc + 2, sum1);
 }
 
-void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_8(const FLAC__real data[], uint32_t data_len, uint32_t lag, FLAC__real autoc[])
+void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_8(const FLAC__real data[], uint32_t data_len, uint32_t lag, double autoc[])
 {
     int i;
     int limit = data_len - 8;
-    float32x4_t sum0 = vdupq_n_f32(0.0f);
-    float32x4_t sum1 = vdupq_n_f32(0.0f);
+    float64x2_t sum0 = vdupq_n_f64(0.0f);
+    float64x2_t sum1 = vdupq_n_f64(0.0f);
+    float64x2_t sum2 = vdupq_n_f64(0.0f);
+    float64x2_t sum3 = vdupq_n_f64(0.0f);
+    float32x4_t sumtemp = vdupq_n_f32(0.0f);
 
     (void)lag;
     FLAC__ASSERT(lag <= 8);
@@ -108,9 +103,14 @@ void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_8(const FLAC__real data[]
         float32x4_t d, d0, d1;
         d0 = vld1q_f32(data + i);
         d1 = vld1q_f32(data + i + 4);
-        d = shufffleVector(d0);
-        sum0 = vaddq_f32(sum0, vmulq_f32(d0, d));
-        sum1 = vaddq_f32(sum1, vmulq_f32(d1, d));
+        d = vdupq_n_f32(vgetq_lane_f32(d0, 0));
+
+        sumtemp = vmulq_f32(d0, d);
+        sum0 = vaddq_f64(sum0, vcvt_f64_f32(vget_low_f32(sumtemp)));
+        sum1 = vaddq_f64(sum1, vcvt_f64_f32(vget_high_f32(sumtemp)));
+        sumtemp = vmulq_f32(d1, d);
+        sum2 = vaddq_f64(sum2, vcvt_f64_f32(vget_low_f32(sumtemp)));
+        sum3 = vaddq_f64(sum3, vcvt_f64_f32(vget_high_f32(sumtemp)));
     }
 
     {
@@ -120,32 +120,39 @@ void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_8(const FLAC__real data[]
         if (limit < 0)
             limit = 0;
 
-        for (; i < (long)data_len; i++)
+        for (i = data_len - 1; i >= limit; i--)
         {
-            float32x4_t d = vld1q_lane_f32(data + i, vdupq_n_f32(0.0f), 0);
-            d = shufffleVector(d);
-
+            float32x4_t d = vdupq_n_f32(data[i]);
             d1 = shufffleVector_3012(d1);
             d0 = shufffleVector_3012(d0);
-
             d1 = copyLane(d1, d0);
             d0 = copyLane(d0, d);
 
-            sum1 = vaddq_f32(sum1, vmulq_f32(d, d1));
-            sum0 = vaddq_f32(sum0, vmulq_f32(d, d0));
+            sumtemp = vmulq_f32(d0, d);
+            sum0 = vaddq_f64(sum0, vcvt_f64_f32(vget_low_f32(sumtemp)));
+            sum1 = vaddq_f64(sum1, vcvt_f64_f32(vget_high_f32(sumtemp)));
+            sumtemp = vmulq_f32(d1, d);
+            sum2 = vaddq_f64(sum2, vcvt_f64_f32(vget_low_f32(sumtemp)));
+            sum3 = vaddq_f64(sum3, vcvt_f64_f32(vget_high_f32(sumtemp)));
         }
     }
-    vst1q_f32(autoc, sum0);
-    vst1q_f32(autoc + 4, sum1);
+    vst1q_f64(autoc, sum0);
+    vst1q_f64(autoc + 2, sum1);
+    vst1q_f64(autoc + 4, sum2);
+    vst1q_f64(autoc + 6, sum3);
 }
 
-void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_12(const FLAC__real data[], uint32_t data_len, uint32_t lag, FLAC__real autoc[])
+void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_12(const FLAC__real data[], uint32_t data_len, uint32_t lag, double autoc[])
 {
     int i;
     int limit = data_len - 12;
-    float32x4_t sum0 = vdupq_n_f32(0.0f);
-    float32x4_t sum1 = vdupq_n_f32(0.0f);
-    float32x4_t sum2 = vdupq_n_f32(0.0f);
+    float64x2_t sum0 = vdupq_n_f64(0.0f);
+    float64x2_t sum1 = vdupq_n_f64(0.0f);
+    float64x2_t sum2 = vdupq_n_f64(0.0f);
+    float64x2_t sum3 = vdupq_n_f64(0.0f);
+    float64x2_t sum4 = vdupq_n_f64(0.0f);
+    float64x2_t sum5 = vdupq_n_f64(0.0f);
+    float32x4_t sumtemp = vdupq_n_f32(0.0f);
 
     (void)lag;
     FLAC__ASSERT(lag <= 12);
@@ -157,10 +164,17 @@ void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_12(const FLAC__real data[
         d0 = vld1q_f32(data + i);
         d1 = vld1q_f32(data + i + 4);
         d2 = vld1q_f32(data + i + 8);
-        d = shufffleVector(d0);
-        sum0 = vaddq_f32(sum0, vmulq_f32(d0, d));
-        sum1 = vaddq_f32(sum1, vmulq_f32(d1, d));
-        sum2 = vaddq_f32(sum2, vmulq_f32(d2, d));
+        d = vdupq_n_f32(vgetq_lane_f32(d0, 0));
+
+        sumtemp = vmulq_f32(d0, d);
+        sum0 = vaddq_f64(sum0, vcvt_f64_f32(vget_low_f32(sumtemp)));
+        sum1 = vaddq_f64(sum1, vcvt_f64_f32(vget_high_f32(sumtemp)));
+        sumtemp = vmulq_f32(d1, d);
+        sum2 = vaddq_f64(sum2, vcvt_f64_f32(vget_low_f32(sumtemp)));
+        sum3 = vaddq_f64(sum3, vcvt_f64_f32(vget_high_f32(sumtemp)));
+        sumtemp = vmulq_f32(d2, d);
+        sum4 = vaddq_f64(sum4, vcvt_f64_f32(vget_low_f32(sumtemp)));
+        sum5 = vaddq_f64(sum5, vcvt_f64_f32(vget_high_f32(sumtemp)));
     }
 
     {
@@ -171,37 +185,48 @@ void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_12(const FLAC__real data[
         if (limit < 0)
             limit = 0;
 
-        for (; i < (long)data_len; i++)
+        for (i = data_len - 1; i >= limit; i--)
         {
-            float32x4_t d = vld1q_lane_f32(data + i, vdupq_n_f32(0.0f), 0);
-            d = shufffleVector(d);
-
+            float32x4_t d = vdupq_n_f32(data[i]);
             d2 = shufffleVector_3012(d2);
             d1 = shufffleVector_3012(d1);
             d0 = shufffleVector_3012(d0);
-
             d2 = copyLane(d2, d1);
             d1 = copyLane(d1, d0);
             d0 = copyLane(d0, d);
 
-            sum2 = vaddq_f32(sum2, vmulq_f32(d, d2));
-            sum1 = vaddq_f32(sum1, vmulq_f32(d, d1));
-            sum0 = vaddq_f32(sum0, vmulq_f32(d, d0));
+            sumtemp = vmulq_f32(d0, d);
+            sum0 = vaddq_f64(sum0, vcvt_f64_f32(vget_low_f32(sumtemp)));
+            sum1 = vaddq_f64(sum1, vcvt_f64_f32(vget_high_f32(sumtemp)));
+            sumtemp = vmulq_f32(d1, d);
+            sum2 = vaddq_f64(sum2, vcvt_f64_f32(vget_low_f32(sumtemp)));
+            sum3 = vaddq_f64(sum3, vcvt_f64_f32(vget_high_f32(sumtemp)));
+            sumtemp = vmulq_f32(d2, d);
+            sum4 = vaddq_f64(sum4, vcvt_f64_f32(vget_low_f32(sumtemp)));
+            sum5 = vaddq_f64(sum5, vcvt_f64_f32(vget_high_f32(sumtemp)));
         }
     }
-    vst1q_f32(autoc, sum0);
-    vst1q_f32(autoc + 4, sum1);
-    vst1q_f32(autoc + 8, sum2);
+    vst1q_f64(autoc, sum0);
+    vst1q_f64(autoc + 2, sum1);
+    vst1q_f64(autoc + 4, sum2);
+    vst1q_f64(autoc + 6, sum3);
+    vst1q_f64(autoc + 8, sum4);
+    vst1q_f64(autoc + 10, sum5);
 }
 
-void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_16(const FLAC__real data[], uint32_t data_len, uint32_t lag, FLAC__real autoc[])
+void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_16(const FLAC__real data[], uint32_t data_len, uint32_t lag, double autoc[])
 {
     int i;
     int limit = data_len - 16;
-    float32x4_t sum0 = vdupq_n_f32(0.0f);
-    float32x4_t sum1 = vdupq_n_f32(0.0f);
-    float32x4_t sum2 = vdupq_n_f32(0.0f);
-    float32x4_t sum3 = vdupq_n_f32(0.0f);
+    float64x2_t sum0 = vdupq_n_f64(0.0f);
+    float64x2_t sum1 = vdupq_n_f64(0.0f);
+    float64x2_t sum2 = vdupq_n_f64(0.0f);
+    float64x2_t sum3 = vdupq_n_f64(0.0f);
+    float64x2_t sum4 = vdupq_n_f64(0.0f);
+    float64x2_t sum5 = vdupq_n_f64(0.0f);
+    float64x2_t sum6 = vdupq_n_f64(0.0f);
+    float64x2_t sum7 = vdupq_n_f64(0.0f);
+    float32x4_t sumtemp = vdupq_n_f32(0.0f);
 
     (void)lag;
     FLAC__ASSERT(lag <= 16);
@@ -214,11 +239,19 @@ void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_16(const FLAC__real data[
         d1 = vld1q_f32(data + i + 4);
         d2 = vld1q_f32(data + i + 8);
         d3 = vld1q_f32(data + i + 12);
-        d = shufffleVector(d0);
-        sum0 = vaddq_f32(sum0, vmulq_f32(d0, d));
-        sum1 = vaddq_f32(sum1, vmulq_f32(d1, d));
-        sum2 = vaddq_f32(sum2, vmulq_f32(d2, d));
-        sum3 = vaddq_f32(sum3, vmulq_f32(d3, d));
+        d = vdupq_n_f32(vgetq_lane_f32(d0, 0));
+        sumtemp = vmulq_f32(d0, d);
+        sum0 = vaddq_f64(sum0, vcvt_f64_f32(vget_low_f32(sumtemp)));
+        sum1 = vaddq_f64(sum1, vcvt_f64_f32(vget_high_f32(sumtemp)));
+        sumtemp = vmulq_f32(d1, d);
+        sum2 = vaddq_f64(sum2, vcvt_f64_f32(vget_low_f32(sumtemp)));
+        sum3 = vaddq_f64(sum3, vcvt_f64_f32(vget_high_f32(sumtemp)));
+        sumtemp = vmulq_f32(d2, d);
+        sum4 = vaddq_f64(sum4, vcvt_f64_f32(vget_low_f32(sumtemp)));
+        sum5 = vaddq_f64(sum5, vcvt_f64_f32(vget_high_f32(sumtemp)));
+        sumtemp = vmulq_f32(d3, d);
+        sum6 = vaddq_f64(sum6, vcvt_f64_f32(vget_low_f32(sumtemp)));
+        sum7 = vaddq_f64(sum7, vcvt_f64_f32(vget_high_f32(sumtemp)));
     }
 
     {
@@ -230,11 +263,9 @@ void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_16(const FLAC__real data[
         if (limit < 0)
             limit = 0;
 
-        for (; i < (long)data_len; i++)
+        for (i = data_len - 1; i >= limit; i--)
         {
-            float32x4_t d = vld1q_lane_f32(data + i, vdupq_n_f32(0.0f), 0);
-            d = shufffleVector(d);
-
+            float32x4_t d = vdupq_n_f32(data[i]);
             d3 = shufffleVector_3012(d3);
             d2 = shufffleVector_3012(d2);
             d1 = shufffleVector_3012(d1);
@@ -245,16 +276,28 @@ void FLAC__lpc_compute_autocorrelation_intrin_neon_lag_16(const FLAC__real data[
             d1 = copyLane(d1, d0);
             d0 = copyLane(d0, d);
 
-            sum3 = vaddq_f32(sum3, vmulq_f32(d, d3));
-            sum2 = vaddq_f32(sum2, vmulq_f32(d, d2));
-            sum1 = vaddq_f32(sum1, vmulq_f32(d, d1));
-            sum0 = vaddq_f32(sum0, vmulq_f32(d, d0));
+            sumtemp = vmulq_f32(d0, d);
+            sum0 = vaddq_f64(sum0, vcvt_f64_f32(vget_low_f32(sumtemp)));
+            sum1 = vaddq_f64(sum1, vcvt_f64_f32(vget_high_f32(sumtemp)));
+            sumtemp = vmulq_f32(d1, d);
+            sum2 = vaddq_f64(sum2, vcvt_f64_f32(vget_low_f32(sumtemp)));
+            sum3 = vaddq_f64(sum3, vcvt_f64_f32(vget_high_f32(sumtemp)));
+            sumtemp = vmulq_f32(d2, d);
+            sum4 = vaddq_f64(sum4, vcvt_f64_f32(vget_low_f32(sumtemp)));
+            sum5 = vaddq_f64(sum5, vcvt_f64_f32(vget_high_f32(sumtemp)));
+            sumtemp = vmulq_f32(d3, d);
+            sum6 = vaddq_f64(sum6, vcvt_f64_f32(vget_low_f32(sumtemp)));
+            sum7 = vaddq_f64(sum7, vcvt_f64_f32(vget_high_f32(sumtemp)));
         }
     }
-    vst1q_f32(autoc, sum0);
-    vst1q_f32(autoc + 4, sum1);
-    vst1q_f32(autoc + 8, sum2);
-    vst1q_f32(autoc + 12, sum3);
+    vst1q_f64(autoc, sum0);
+    vst1q_f64(autoc + 2, sum1);
+    vst1q_f64(autoc + 4, sum2);
+    vst1q_f64(autoc + 6, sum3);
+    vst1q_f64(autoc + 8, sum4);
+    vst1q_f64(autoc + 10, sum5);
+    vst1q_f64(autoc + 12, sum6);
+    vst1q_f64(autoc + 14, sum7);
 }
 #endif /* FLAC__CPU_ARM && FLAC__HAS_ARCH64INTRIN */
 #endif /* FLAC__NO_ASM */
